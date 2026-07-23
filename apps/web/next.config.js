d3cbf221ab27bd8f1b14233c9c2846819a73f575
Ben NGuyen <your-email@gmail.com>
2026-07-23 12:38:28 -0700

    fix: enforce to run an instance of react

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,22 +1,10 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
   experimental: {
     serverComponentsExternalPackages: ['stripe']
-  },
-  webpack: (config) => {
-    // Force Webpack to resolve single instance of React & React-dom
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      react: path.resolve(__dirname, 'node_modules/react'),
-      'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
-    };
-    // config.resolve.modules.push(path.resolve('../../node_modules'));
-    return config;
   }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
